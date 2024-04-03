c853e2ed404180fdc261bd736c7870b00dd44a35
Karan <karna.eze@gmail.com>
2024-04-03 02:03:11 -0400

    Now Mongo Db Configuartion enabled

--- src/main/java/com/CosmosMongoApp/CosmosMongoAppApplication.java
+++ src/main/java/com/CosmosMongoApp/CosmosMongoAppApplication.java
@@ -1,9 +1,10 @@
 package com.CosmosMongoApp;
 
+import com.CosmosMongoApp.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {UserController.class})
 public class CosmosMongoAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/CosmosMongoApp/controller/UserController.java
+++ src/main/java/com/CosmosMongoApp/controller/UserController.java
@@ -3,9 +3,13 @@ package com.CosmosMongoApp.controller;
 import com.CosmosMongoApp.repo.UserRepo;
 import com.CosmosMongoApp.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -14,9 +18,14 @@ public class UserController {
     private UserRepo userRepo;
 
     @PostMapping("/user")
-    public String user(@RequestBody UserModel model){
-        userRepo.save(model);
-        return "User Data Saved....!";
+    public UserModel user(@RequestBody UserModel model){
+        model.setId(UUID.randomUUID().toString());
+        return  userRepo.save(model);
+
+    }
+    @GetMapping("/userInfo")
+    public List<UserModel> getUsers(){
+        return userRepo.findAll();
     }
 
 }
